80f68a9e943cc600e3451944468683e7caee84a0
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-06 12:47:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import './src/command.js';
+const note = process.argv[2];
+
+const newNote = {
+    content: note,
+    id:Date.now()
+};
+console.log(newNote);
+console.log('hello world')
